2523256417cc3cd923090ed3bf320b179ff318b3
Eric Gelinas <eric@standardpixel.com>
2009-08-30 15:17:51 -0700

    added timeline controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090829205752) do
+ActiveRecord::Schema.define(:version => 20090830195105) do
 
   create_table "cliques", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(:version => 20090829205752) do
     t.datetime "updated_at"
     t.string   "twitter"
     t.integer  "clique_id"
+  end
+
+  create_table "timelines", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
